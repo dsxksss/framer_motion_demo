f1199e47d7da121b1f040dbd6b8d2f2baba49c98
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-11-18 23:00:20 +0800

    优化demo代码

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from "react";
 import { motion, MotionConfig } from "framer-motion";
 
 function App() {
-  const [tag, setTag] = useState(true);
+  const [tag, setTag] = useState(0);
+
+  const texts = [
+    `Lorem ipsum dolor sit amet consectetur 
+    adipisicing elit. Quos omnis fugit voluptate, 
+    ipsa eligendi est cum nemo earum architecto 
+    natusdolorem optio molestias numquam autem 
+    consectetur maiores vel magnivero.`,
+    `quidem laborum, tempora unde. Vitae iusto 
+    laboriosamarchitectoexercitationem!`,
+    `optio molestias numquam autem consectetur 
+    maiores vel magni vero.`,
+    `a`,
+    `b`,
+    `c`,
+    `d`,
+  ];
 
   const ResizablePanel = ({ children }) => {
     return (
@@ -14,26 +30,16 @@ function App() {
 
   return (
     <MotionConfig transition={{ duration: 0.5 }}>
-      <ResizablePanel>
-        {tag ? (
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos omnis,
-            fugit voluptate, ipsa eligendi est cum nemo earum architecto natus
-            dolorem optio molestias numquam autem consectetur maiores vel magni
-            vero.
-          </p>
-        ) : (
-          <p>
-            optio molestias numquam autem consectetur maiores vel magni vero.
-          </p>
-        )}
-      </ResizablePanel>
+      <ResizablePanel>{texts[tag]}</ResizablePanel>
       <motion.button
-        className={"px-8 py-2 bg-slate-800 text-white rounded-[5px]"}
+        className={"px-8 py-2 bg-slate-700 text-white rounded-[5px]"}
         whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
         whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
         whileInView={{ scale: 1, transition: { duration: 0.2 } }}
-        onClick={() => setTag(!tag)}
+        onClick={() => {
+          if (tag >= texts.length - 1) setTag(0);
+          else setTag(tag + 1);
+        }}
       >
         切换
       </motion.button>
