1199bab7edcfb4b97c7c03ab2b0b6d17a2be2547
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-11-25 17:45:21 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
 import { useState } from "react";
-import { motion, MotionConfig } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-  const [tag, setTag] = useState(0);
+  const [item, setitem] = useState([
+    "list item 1",
+    "list item 2",
+    "list item 3",
+  ]);
 
-  const texts = [
-    `Lorem ipsum dolor sit amet consectetur 
-    adipisicing elit. Quos omnis fugit voluptate, 
-    ipsa eligendi est cum nemo earum architecto 
-    natusdolorem optio molestias numquam autem 
-    consectetur maiores vel magnivero.`,
-    `quidem laborum, tempora unde. Vitae iusto 
-    laboriosamarchitectoexercitationem!`,
-    `optio molestias numquam autem consectetur 
-    maiores vel magni vero.`,
-    `a`,
-    `b`,
-    `c`,
-    `d`,
-  ];
+  function addItem(item) {
+    setitem((items) => [...items, `list item  ${item}`]);
+  }
 
-  const ResizablePanel = ({ children }) => {
-    return (
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <div style={{ height: "50px" }}>{children}</div>
-      </motion.div>
-    );
-  };
+  function removeItem(item) {
+    setitem((items) => items.filter((i) => i !== item));
+  }
 
   return (
-    <MotionConfig transition={{ duration: 0.5 }}>
-      <ResizablePanel>{texts[tag]}</ResizablePanel>
-      <motion.button
-        className={"px-8 py-2 bg-slate-700 text-white rounded-[5px]"}
-        whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-        whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-        whileInView={{ scale: 1, transition: { duration: 0.2 } }}
-        onClick={() => {
-          if (tag >= texts.length - 1) setTag(0);
-          else setTag(tag + 1);
-        }}
-      >
-        切换
-      </motion.button>
-    </MotionConfig>
+    <div className="p-20">
+      <div>
+        <button
+          className="border rounded w-8 h-8"
+          onClick={() => addItem(item.length + 1)}
+        >
+          Add
+        </button>
+        <ul>
+          <AnimatePresence>
+            {item.map((e) => (
+              <motion.li
+                key={e}
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                  layout: { duration: 0.3 },
+                  opacity: { duration: 0.1 },
+                }}
+                className="flex justify-between items-center py-2 border-b"
+              >
+                <span>{e}</span>
+                <button
+                  className="border rounded w-8 h-8"
+                  onClick={() => removeItem(e)}
+                >
+                  &times;
+                </button>
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </ul>
+      </div>
+    </div>
   );
 }
 
